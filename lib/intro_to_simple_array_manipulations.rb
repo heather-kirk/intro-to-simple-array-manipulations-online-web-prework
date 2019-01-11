8d94f953c52201866701001bfa0dd13f72720f23
Heather Kirk <hskirk@gmail.com>
2019-01-11 01:24:13 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,10 +1,12 @@
 def using_push(countries_in_western_africa)
  countries_in_western_africa= ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
-  countries_in_western_africa(next_country)
+  countries_in_western_africa.push
 end
 
-def  using_unshift(array)
-  array = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
+def using_unshift(neighborhoods_in_northwest_brooklyn)
+  neighborhoods_in_northwest_brooklyn = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
   new_neighborhood = "Brooklyn Heights"
-  new
+  neighborhoods_in_northwest_brooklyn.unshift
+end
+
