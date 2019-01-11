2476abdc32cde898950e2a898e310915c13321b7
Heather Kirk <hskirk@gmail.com>
2019-01-11 01:21:54 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,9 +1,10 @@
-def using_push(array)
-  array = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
+def using_push(artists)
+  artists = ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"]
   next_country = "Niger"
   artists.push(next_country)
 end
 
 def  using_unshift(array)
   array = ["Brooklyn Yards", "Cadman Plaza", "Clinton Hill", "Downtown Brooklyn", "DUMBO"]
-  
+  new_neighborhood = "Brooklyn Heights"
+  new
