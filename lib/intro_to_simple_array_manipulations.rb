754a86638f2916ce242a74bcf1668a9a56339c6d
Heather Kirk <hskirk@gmail.com>
2019-01-11 01:30:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push(array, countries_in_western_africa= ["Benin", "Carpe Verde", "Gambia", "Liberia", "Mali"])
   next_country = "Niger"
-  using_push.push(next_country)
+  using_push.push("Niger")
 end
 
 def using_unshift(array, neighborhoods_in_northwest_brooklyn)
